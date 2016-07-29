ea299b8ea8d57fb41388e37c6059c1a4c0fb1679
ftarlaci <fatmatarlaci@Fatmas-iMac.attlocal.net>
2016-07-29 13:10:24 -0500

    sql

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE burgers_db;
+USE burgers_db;
+
+CREATE TABLE `burgers` (
+	`id` INTEGER(20) NOT NULL AUTO_INCREMENT,
+	`burger_name` VARCHAR(255) NOT NULL,
+	`devoured` BOOLEAN NOT NULL DEFAULT '0', '1',
+	`date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, 
+	PRIMARY KEY (`id`)
+	);
---
+++ server.js
@@ -0,0 +1,4 @@
+var express = require('express');
+var method-override = require('method-override');
+var bodyParser = require('body-parser');
+
