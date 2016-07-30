f959acb16d07189e772a237b9e7629309f218503
ftarlaci <ftarlaci@utexas.edu>
2016-07-30 12:22:39 -0500

    main.handlebars fixed

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
 	port: 3306,
-	host: "127.0.0.1",
-	user: "root",
-	password: "",
+	host: 'localhost',
+	user: 'root',
+	password: '',
 	database: "burgers_db"
 });
 
