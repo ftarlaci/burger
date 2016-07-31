93e39f8715ed565e0bbe9ed406512a535e55bfe2
ftarlaci <ftarlaci@utexas.edu>
2016-07-31 12:31:30 -0500

    jawsdb keys

--- config/connection.js
+++ config/connection.js
@@ -7,6 +7,14 @@ var connection = mysql.createConnection({
 	database: "nvoc65462s60dt0h"
 });
 
+// var connection = mysql.createConnection({
+// 	port: 3306,
+// 	host: 'localhost',
+// 	user: 'root',
+// 	password: '',
+// 	database: "burgers_db"
+// });
+
 connection.connect(function (err){
 	if(err) {
 		console.log("Connection error:" + err.stack);
--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,4 @@
-CREATE TABLE burgers_db;
-USE burgers_db;
-
-CREATE TABLE `burgers` (
+CREATE TABLE burgers (
 	`id` INTEGER(20) NOT NULL AUTO_INCREMENT,
 	`burger_name` VARCHAR(255) NOT NULL,
 	`devoured` BOOLEAN NOT NULL DEFAULT '0', '1',
